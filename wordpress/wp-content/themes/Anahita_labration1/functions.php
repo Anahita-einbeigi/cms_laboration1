1fdbfdc2e1c3694a229bc94c96a85e5cae0951dc
Ania-Beigi <anahita.einbeigi@gmail.com>
2024-12-03 13:57:26 +0100

    nya ändringar i min index.php och andra filer

--- wordpress/wp-content/themes/Anahita_labration1/functions.php
+++ wordpress/wp-content/themes/Anahita_labration1/functions.php
@@ -24,9 +24,17 @@
 
 
     function load_theme_styles() {
-        wp_enqueue_style('main-styles', get_stylesheet_uri());
-    }
+        wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/css/bootstrap.css', array(), null, 'all');
+        wp_enqueue_style('fontawesome-css', get_template_directory_uri() . '/css/font-awesome.css', array(), null, 'all');
+        wp_enqueue_style('theme-style', get_template_directory_uri() . '/css/style.css', array('bootstrap-css', 'fontawesome-css'), '1.0.0', 'all');   
+     }
     add_action('wp_enqueue_scripts', 'load_theme_styles');
     
-    
+
+    function load_theme_scripts() {
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('custom-script', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0.0', true);
+    }
+    add_action('wp_enqueue_scripts', 'load_theme_scripts');
+
 ?>
---
+++ wordpress/wp-content/themes/Anahita_labration1/page-contact.php
@@ -0,0 +1,11 @@
+<?php
+get_header();
+?>
+
+<main>
+    <?php echo do_shortcode('[wpforms id="77"]'); ?>
+</main>
+
+<?php
+get_footer();
+?>
